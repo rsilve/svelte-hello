f9619b6ab484051f2916d90b8dd978a04bd1a739
Robert Silve <robert@silve.net>
2022-02-28 07:43:05 +0000

    fix vite conf for gitpod

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+let hmr = {};
+if (process.env.GITPOD_GIT_USER_EMAIL) {
+  hmr = { port : 443 }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { hmr },
   plugins: [svelte()]
 })
